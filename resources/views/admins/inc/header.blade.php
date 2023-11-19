7fe35fb9c767250b9e83a2beb810374cf8d11307
brianchemase <brianchemo@gmail.com>
2023-11-19 18:07:44 +0300

    upload files

--- resources/views/admins/inc/header.blade.php
+++ resources/views/admins/inc/header.blade.php
@@ -14,9 +14,29 @@
                 </a>
 
                 <li class="dropdown dropdown-user nav-item"><a class="dropdown-toggle nav-link dropdown-user-link"
-                    href="#" data-toggle="dropdown"> <span class="avatar img-fluid rounded me-1"><img src="{{ asset('adminsdash/img/avatars/avatar.jpg') }}" class="avatar img-fluid rounded me-1"
-                        alt="profile" /> <span class="text-dark">Username</span>
+                    href="#" data-toggle="dropdown"> <span class="avatar img-fluid rounded me-1"><img src="{{ asset('admins/img/avatars/ppt.jpg') }}" class="avatar img-fluid rounded me-1"
+                        alt="profile" /> <span class="text-dark">{{ Auth::user()->name }}</span>
                 </a>
+
+                <div class="dropdown-menu dropdown-menu-right">
+								<a class="dropdown-item" href="#"><i class="align-middle mr-1" data-feather="user"></i> Profile</a>
+								<a class="dropdown-item" href="#"><i class="align-middle mr-1" data-feather="pie-chart"></i> Analytics</a>
+								<div class="dropdown-divider"></div>
+								<a class="dropdown-item" href="#"><i class="align-middle mr-1" data-feather="settings"></i> Settings &
+									Privacy</a>
+								<a class="dropdown-item" href="{{ route('users.index') }}"><i class="align-middle mr-1" data-feather="help-circle"></i> User Managment</a>
+								<a class="dropdown-item" href="#"><i class="align-middle mr-1" data-feather="help-circle"></i> Help Center</a>
+								<div class="dropdown-divider"></div>
+								<a class="dropdown-item" href="{{ route('logout') }}"
+                                       onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                        {{ __('Log out') }}
+                                    </a>
+
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                        @csrf
+                                    </form>
+							</div>
                 
             </li>
         </ul>
